89d1829f0718cd03c62b143864fbdd0820033566
Alexandr <katmai84@gmail.com>
2021-02-02 01:22:08 +0600

    начало

---
+++ handlers/start.py
@@ -0,0 +1,12 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+
+from keyboadrd import menu
+from main import dp
+
+
+@dp.message_handler((CommandStart()))
+async def bot_start(message: types.Message):
+    text = f'Привет, {message.from_user.full_name}!' \
+           f'Пришли несколько фотографий, но не более 10'
+    await message.answer(text, reply_markup=menu)
